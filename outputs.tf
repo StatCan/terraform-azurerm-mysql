cab2c713d5e22bdde1040b9dff430577b1bfc9c9
William Hearn <william.hearn@canada.ca>
2022-06-24 09:05:28 -0400

    feat(outputs): Add storage account to outputs

--- outputs.tf
+++ outputs.tf
@@ -21,3 +21,11 @@ output "identity_tenant_id" {
 output "identity_object_id" {
   value = azurerm_mysql_server.mysql.identity[0].principal_id
 }
+
+output "storage_account_id" {
+  value = length(azurerm_storage_account.mysql) > 0 ? azurerm_storage_account.mysql[0].id : null
+}
+
+output "storage_account_name" {
+  value = length(azurerm_storage_account.mysql) > 0 ? azurerm_storage_account.mysql[0].name : null
+}
